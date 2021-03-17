d53181802b87b2042d88125f5bff6788c7c8fa49
mihui <mihui@cn.ibm.com>
2021-03-17 12:31:58 +0800

    Completed coding

---
+++ index.js
@@ -0,0 +1,56 @@
+/**
+ * Wait until it matches a given condition
+ */
+const waitit = {
+  STATUS: {
+    COMPLETED: 'COMPLETED',
+    CANCELLED: 'CANCELLED',
+    TIMEOUT: 'TIMEOUT'
+  },
+  cancel: false,
+  /**
+   * Start waiting for the condition or timeout
+   * 
+   * @param {object} options Options
+   * 
+   *  - check: check for the given condition
+   *  - interval: default: 100 (0.1 second)
+   *  - maxTicks: default 100 (equals to 10 seconds)
+   * @returns 
+   */
+  start: (options) => {
+    let t = 0;
+    let ticks = 0;
+    options.interval = options.interval || 1000;
+    options.maxTicks = options.maxTicks || 10;
+    options.check = options.check || (() => { return true });
+    options.tick = options.tick || ((tick) => {
+      console.log(tick);
+    });
+
+    return new Promise((resolve, reject) => {
+      t = setInterval(() => {
+        // Cancel
+        if(waitit.cancel) {
+          clearInterval(t);
+          reject({ code: waitit.STATUS.CANCELLED });
+        }
+        console.log(ticks);
+        // Check condition
+        if(options.check()) { 
+          clearInterval(t);
+          resolve({ code: waitit.STATUS.COMPLETED });
+        }
+        else if(ticks++ > options.maxTicks) {
+          clearInterval(t);
+          reject({ code: waitit.STATUS.TIMEOUT });
+        }
+      }, options.interval);
+    });
+  },
+  stop: () => {
+    waitit.cancel = true;
+  }
+};
+
+module.exports = waitit;
---
+++ test/test.wait.js
@@ -0,0 +1,47 @@
+var assert = require('assert');
+const waitit = require('../index');
+
+describe('Test Conditional Waiting', () => {
+  it('Should be good', async () => {
+    let condition = false;
+    setTimeout(() => {
+      condition = true;
+    }, 1);
+    try {
+      const wait = await waitit.start({
+        check: () => {
+          return condition;
+        },
+        maxTicks: 5
+      });
+      console.log(wait);
+      assert.strictEqual(waitit.STATUS.COMPLETED, wait.code);
+    }
+    catch(error) {
+      console.log(error);
+      assert.strictEqual(waitit.STATUS.TIMEOUT, error.code);
+    }
+  });
+
+  it('Should be timeout', async () => {
+    let condition = false;
+    setTimeout(() => {
+      condition = true;
+    }, 10000);
+    try {
+      const wait = await waitit.start({
+        check: () => {
+          return condition;
+        },
+        maxTicks: 5
+      });
+      console.log(wait);
+      assert.strictEqual(waitit.STATUS.COMPLETED, wait.code);
+    }
+    catch(error) {
+      console.log(error);
+      assert.strictEqual(waitit.STATUS.TIMEOUT, error.code);
+    }
+  });
+});
+
